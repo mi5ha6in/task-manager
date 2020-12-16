f26fdf0281a26550a52b2875076a586cedd68780
mi5ha6in <mi5ha6in@gmail.com>
2020-12-16 12:59:46 +0300

    continue refactoring

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,20 @@
+import dayjs from "dayjs";
+
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+export const isTaskExpired = (dueDate) => {
+  return dueDate === null ? false : dayjs().isAfter(dueDate, `D`);
+};
+
+export const isTaskRepeating = (repeating) => {
+  return Object.values(repeating).some(Boolean);
+};
+
+export const humanizeTaskDueDate = (dueDate) => {
+  return dayjs(dueDate).format(`D MMMM`);
 };
