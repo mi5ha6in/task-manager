d08376a0542803ff019e3f209a022c1b3621f78a
mi5ha6in <mi5ha6in@gmail.com>
2020-12-23 18:57:25 +0300

    fix bug in generateTask

--- src/mock/task.js
+++ src/mock/task.js
@@ -53,9 +53,9 @@ export const generateTask = () => {
     : {
       mo: false,
       tu: false,
-      we: Boolean(getRandomInteger(0, 1)),
+      we: false,
       th: false,
-      fr: Boolean(getRandomInteger(0, 1)),
+      fr: false,
       sa: false,
       su: false
     };
