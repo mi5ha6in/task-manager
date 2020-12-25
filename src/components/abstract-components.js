c876d8a34bafbf30db91fe1d766bc926ae8a5cb3
mi5ha6in <mi5ha6in@gmail.com>
2020-12-25 13:17:05 +0300

    moves common methods into an abstract class

--- src/components/abstract-components.js
+++ src/components/abstract-components.js
@@ -1,7 +1,27 @@
+import {createElement} from "../utils.js";
+
 export default class AbstractComponents {
   constructor() {
     if (new.target === AbstractComponents) {
       throw new Error(`Can't instantiate AbstractComponents, only concrete one`);
     }
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error(`Abstract method not implemented: getTemplate`);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
