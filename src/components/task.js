1c67cfdc693d4b3b35ad3dfdf1a12ec4253be0ef
mi5ha6in <mi5ha6in@gmail.com>
2021-01-15 16:39:06 +0300

    fix eslint

--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,5 @@
 import {MONTH_NAMES} from "../const.js";
-import {createElement,formatTime} from "../utils.js";
+import {createElement, formatTime} from "../utils.js";
 
 const createTaskTemplate = (task) => {
   const {description, dueDate, color, repeatingDays, isArchive, isFavorite} = task;
