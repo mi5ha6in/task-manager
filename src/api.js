760f6c5aa558e68f35d4b9018ca040be3fc79b8d
mi5ha6in <mi5ha6in@gmail.com>
2021-02-02 18:01:02 +0300

    adds and setting API

---
+++ src/api.js
@@ -0,0 +1,15 @@
+const API = class {
+  constructor(authorization) {
+    this._authorization = authorization;
+  }
+
+  getTasks() {
+    const headers = new Headers();
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`https://11.ecmascript.pages.academy/task-manager/tasks`, {headers})
+      .then((response) => response.json());
+  }
+};
+
+export default API;
