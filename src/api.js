435241ca459727a5e7fa3c1f8549e27e101ad162
mi5ha6in <mi5ha6in@gmail.com>
2021-02-04 18:18:43 +0300

    implements updating data on the server

--- src/api.js
+++ src/api.js
@@ -1,5 +1,13 @@
 import Task from "./models/task.js";
 
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`)
+  }
+};
+
 const API = class {
   constructor(authorization) {
     this._authorization = authorization;
@@ -10,8 +18,24 @@ const API = class {
     headers.append(`Authorization`, this._authorization);
 
     return fetch(`https://11.ecmascript.pages.academy/task-manager/tasks`, {headers})
+      .then(checkStatus)
       .then((response) => response.json())
       .then(Task.parseTasks);
+  }
+
+  updateTask(id, data) {
+    const headers = new Headers();
+    headers.append(`Authorization`, this._authorization);
+    headers.append(`Content-Type`, `application/json`);
+
+    return fetch(`https://11.ecmascript.pages.academy/task-manager/tasks/${id}`,{
+      method: `PUT`,
+      body: JSON.stringify(data.toRAW()),
+      headers,
+    })
+      .then(checkStatus)
+      .then((response) => response.json())
+      .then(Task.parseTask);
   }
 };
 
